2fb22f68cbe3ab34d2c9597db468b09eb23c3a9d
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-07 04:44:16 -0700

    Update repo with 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * @main - Entry point
+ *
+ * Placing "\" makes it able for me to add another " in the string
+ *
+ * @return - always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
