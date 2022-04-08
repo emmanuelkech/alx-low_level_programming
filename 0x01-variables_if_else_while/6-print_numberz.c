34f4b47d7c83e040b27c690056bfef4228621550
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-08 07:24:30 -0700

    Update repo with 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
