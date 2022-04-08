51178488953802f427c506c6e09be863c0c192dd
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-08 07:45:24 -0700

    Update repo with 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers with commas and spaces
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
