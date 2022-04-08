7aba20d9414fad8f0af9f9633045abd158c3730b
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-08 06:48:28 -0700

    Update repo with 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints the alphabets without q and e
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
