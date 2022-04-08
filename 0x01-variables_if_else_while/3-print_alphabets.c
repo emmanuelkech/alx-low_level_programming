59cf7fd7aa6aa9386fd530aa091279b9a434e0e7
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-08 06:34:17 -0700

    Update repo with 3-print_alphabets.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,12 @@
 
 int main(void)
 {
-	char low[26] = "abcdefghijklmnopqrstuvwxyz";
-	char cap[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char low[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
-		putchar(low[i]);
-		putchar(cap[i]);
+		putchar(alph[i]);
 	}
 	putchar('\n');
 
