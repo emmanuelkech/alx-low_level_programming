7686d1972dd7c20c873cbdec8ab2fb73355fb0ca
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-12 07:44:43 -0700

    Update repo with 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @i: an input character
+ *
+ * Return: absolute value of i
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (r * -1);
