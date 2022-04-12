02149eafc668c02ef0075a335c4061c3263ad9ea
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-12 05:05:03 -0700

    Update repo with 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: an input character
+ *
+ * Return: 1 if letter || 0 if not letter
+ */
+
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				letter = 1;
+		}
+	}
+	return (0);
+}
