49915aff8029e2b29b88c6c8dcb6a4e8ebadbc23
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-12 04:37:29 -0700

    Update repo with 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times,
+ * in lowecase, followed by a new line.
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
