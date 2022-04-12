198558e321fe965f094e4ce0eb6704188409a65f
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-12 05:30:46 -0700

    Update repo with 5-sign.c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: an input character
+ *
+ * Return: 1 if > 0 and prints + || 
+ * 0 if == 0 and prints 0 || 
+ * -1 if < 0 and prints -
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+
+	return (value);
+}
