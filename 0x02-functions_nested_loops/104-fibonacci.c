e8413f9a6e5e9c2cd9e6a53a97fa66ce76c1a34f
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-12 10:51:49 -0700

    Update repo with 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples
- * of 3 or 5 below 1024, followed by a new line
+ * main - prints the first 98 fibonacci numbers,
+ * starting with 1 and 2
  *
  * Return: 0 (success)
+ *
  */
 
 int main(void)
 {
-	int i = 0;
-	unsigned long int a = 0, b = 1, next = 0;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	while (i < 98)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		next = a + b;
-		a = b;
-		b = next;
-		printf("%lu", next);
-
-		if (i < 97)
-			printf(", ");
-		i++;
+		pfintf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	putchar('\n');
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
 
 	return (0);
 }
