75274898f8ebbd1ba25c5cd12414135c4800c312
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-12 08:48:37 -0700

    Update repo with 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: an input character
+ *
+ * @b: an input character
+ *
+ * Return: result of a+b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
