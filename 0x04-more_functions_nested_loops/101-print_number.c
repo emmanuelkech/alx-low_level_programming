bc782c7eb25b77dbfe05c8be3988e92791d0a876
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-14 06:08:52 -0700

    Update repo with 100-print_number.c file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,37 @@
 #include "main.h"
+void print_integer(int m);
 
 /**
- * print_number - prints numbers
+ * print_number - prints an integer
  *
  * @n: number to be printed
  */
 
 void print_number(int n)
 {
-	int d = 1, i = 0, ii = 0;
-
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 	{
-		_putchar('_');
-		n = -n;
+		_putchar('-');
+		print_integer(n * -1);
 	}
+	else
+		print_integer(n);
+}
 
-	while (n / d != 0)
-	{
-		d *= 10;
-		i++;
-	}
-	d = d / 10;
+/**
+ * print_integer - for printing n
+ * @m: input unsigned integer
+ */
 
-	while (ii < i)
-	{
-		_putchar('0' + n / d);
-		n = n - (n / d) * d;
-		d = d / 10;
-		ii++;
-	}
+void print_integer(int m)
+{
+	int i = 1000000000;
 
-	if (i == 0)
-		_putchar('0' + n);
+	for (; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
 }
