375cea3d317a37c8e3d6d8fe96ccb26a09b5b19b
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-14 05:00:07 -0700

    Update repo with 4-print_most_numbers.c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
- * followed by a new line,
- * do not print 2 and 4
+ * do not print 2 and 4,
+ * followed by a new line.
  */
 
 void print_most_numbers(void)
@@ -16,4 +16,4 @@ void print_most_numbers(void)
 			_putchar(i);
 	}
 	_putchar('\n');
-}	
+}
