4dd4c0b471c4e60b2892d2279dbc363742d94d91
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-04-26 03:15:57 -0700

    Update repo with 1-memcpy.c file

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes from memory area src
+ * to memory area dest
+ *
+ * @dest: destination where memory is copied to
+ * @src: memory to be copied
+ * @n: number of bytes from memory area src to be copied
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
