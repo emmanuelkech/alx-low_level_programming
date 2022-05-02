ea53929f5824db0d09ab54a7a8c8879e0211cfc9
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-05-02 04:34:12 -0700

    Update repo with 3-mul.c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,15 +14,18 @@ int main(int argc, char *argv[])
 {
 	int a, b, c;
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		puts("Error");
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a * b;
+		printf("%d\n", c);
+	}
+	else
+	{
+		printf("Error\n");
 		return (1);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	c = a * b;
-	printf("%d\n", c);
 
-	return(0);
+	return (0);
 }
