ece49ced84d5f66cea9aac1bc96f223a6a508751
Emmanuel Ikechukwu <eikechukwu897@gmail.com>
2022-05-24 03:07:47 -0700

    Update repo with 101-hello_holberton.asm file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+; Desc: 64-bit assembly program that prints
+; Hello, Holberton followed by a new line.
+
+          global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
